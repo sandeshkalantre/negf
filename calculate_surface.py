66176badb320da131a6f6c5078701473934f9dfd
Sandesh Kalantre <kalantresandesh@gmail.com>
2016-09-26 16:51:45 +0530

    josephson works

--- calculate_surface.py
+++ calculate_surface.py
@@ -14,7 +14,7 @@ alpha = np.array([[2*t0 - mu,Delta],[np.conj(Delta),-2*t0 + mu]])
 beta = -t0 * tau3
 
 
-def calculate_surface(E,alpha,beta,eta = 0.00001,N = 1000,eps = 0.00001):
+def calculate_surface(E,alpha,beta,eta = 0.00001,N = 10000,eps = 0.000001):
     # N : number of iterations
     g0 = alpha + 1j * eta * np.eye(2)
     g = g0
@@ -25,7 +25,7 @@ def calculate_surface(E,alpha,beta,eta = 0.00001,N = 1000,eps = 0.00001):
         g = 0.5 * (g + g0) 
         if LA.norm(g_last - g)/LA.norm(g_last) < eps:
             #print "--------------------------------------------------------------------------------"
-            print "Calculated in",i,"iterations for E = ",E 
+            #print "Calculated in",i,"iterations for E = ",E 
             #print "alpha =\n",alpha
             #print "g =\n",g
             #print "\n"
@@ -33,7 +33,7 @@ def calculate_surface(E,alpha,beta,eta = 0.00001,N = 1000,eps = 0.00001):
         else:
             g_last = g
     #print "--------------------------------------------------------------------------------"
-    print "Calculated in",N,"iterations for E = ",E 
+    #print "Calculated in",N,"iterations for E = ",E 
     #print "alpha =\n",alpha
     #print "g =\n",g
     #print "\n"
--- josephson.py
+++ josephson.py
@@ -8,18 +8,21 @@ import calculate_surface
 
 def calculate_josephson(g_1,g_2):
     return 4*np.real(g_1[1,0] * g_2[0,1] - g_2[1,0] * g_1[0,1])
+t0 = 10.0
+mu = 100.0
+Delta = 1.0
 
-t0_1 = 4.0
-mu_1 = 10.0
-Delta_1 = 2.0 * np.exp(1j * 0.0)
+t0_1 = t0
+mu_1 = mu
+Delta_1 = Delta * np.exp(1j * 0.0)
 
 alpha_1 = np.array([[2*t0_1 - mu_1,Delta_1],[np.conj(Delta_1),-2*t0_1 + mu_1]])
 beta_1 = -t0_1 * tau3
 
 
-t0_2 = 4.0
-mu_2 = 10.0
-Delta_2 = 2.0 * np.exp(1.0j * 0.0)
+t0_2 = t0
+mu_2 = mu
+Delta_2 = Delta * np.exp(1.0j * 0.0)
 
 alpha_2 = np.array([[2*t0_2 - mu_2,Delta_2],[np.conj(Delta_2),-2*t0_2 + mu_2]])
 beta_2 = -t0_2 * tau3
@@ -27,14 +30,14 @@ beta_2 = -t0_2 * tau3
 #number of points in Energy linspace
 M = 50
 #since the iteration to calculate surface Green's function blows up near the superconducting gap, the energy values are kept lower than that
-E = np.linspace(mu_1-10,mu_1+2,M)
+E = np.linspace(mu_1-2*Delta,mu_1+2*Delta,M)
 #number of points in phase linspace
 N = 100
 phi = np.linspace(0,2*np.pi,N)
 I_J = np.zeros(N)
 
 for i in range(N):
-    print "Calculating for phase difference = ",phi[i]
+    #print "Calculating for phase difference = ",phi[i]
     for j in range(M):
         Delta_2 = 2.0 * np.exp(1.0j * phi[i])
         alpha_2 = np.array([[2*t0_2 - mu_2,Delta_2],[np.conj(Delta_2),-2*t0_1 + mu_2]])
@@ -42,9 +45,15 @@ for i in range(N):
         g_1 = calculate_surface.calculate_surface(E[j], alpha_1,beta_1)
         g_2 = calculate_surface.calculate_surface(E[j], alpha_2,beta_2)
 
-        I_J[i] += calculate_josephson(g_1, g_2)
-    print "\n"
+        I_J[i] += calculate_josephson(g_1, g_2) 
+    #print "\n"
 
-plt.plot(phi,I_J)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(phi,I_J)
+ax.set_xlabel(r"$\Delta \phi$", fontsize=16)
+ax.set_ylabel(r"$I_J$ (Arbitary Units)", fontsize=16)
+ax.set_title("Josephson Effect at zero bias")
+ax.text(0.9,0.9,r"$t = $" + str(t0) + "\n" + r"$\mu = $" + str(mu) + "\n" + r"$|\Delta| = $" + str(np.abs(Delta)),ha="center",va="center",transform=ax.transAxes,fontsize=16) 
 plt.show()
 
