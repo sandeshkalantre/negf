b1ef0346aa9a2b4dadb45793f3c5a58c631c0fc8
Sandesh Kalantre <kalantresandesh@gmail.com>
2016-10-10 13:53:46 +0530

    I_J using the current operator works

---
+++ MATLAB Codes/I_J__phi_using_current_operator.m
@@ -0,0 +1,94 @@
+%calculation of the Josephson current vs phase directly using the current
+%operator
+
+%tolerance for calculation of surface Green's function
+eps = 1e-4;
+
+%0+ to get convergence to retarded Green's function
+eta = 1e-8;
+
+%energies in eV
+t0 = 0.1;
+Delta = 0.008;
+kT = 0.002;
+
+phi_vec = 2*pi*(0:0.05:1);
+I_phi = zeros(2,length(phi_vec));
+
+%tunelling
+M0 = 0.001;
+M = M0 .* eye(2);
+
+for ii = 1:length(phi_vec)
+    phi = phi_vec(ii);
+
+    Delta1 = Delta;
+    alpha1 = [2*t0 Delta1; conj(Delta1) -2*t0];
+    beta1 = -t0* [1 0; 0 -1];
+
+    Delta2 = Delta * exp(1i*phi);
+    alpha2 = [2*t0 Delta2; conj(Delta2) -2*t0];
+    beta2 = -t0* [1 0; 0 -1];
+
+    E = 4 * t0 *(0:0.01:1);
+    I_E = zeros(2,length(E));
+    for jj = 1:length(E)
+        EE = E(jj);
+    
+        %calculation of g iteratively
+        g1 = inv(alpha1);
+        g1_last = inv(alpha1);
+        err = 1;
+        for kk = 1:1000
+            g1 = inv((EE + 1i*eta)*eye(2) - alpha1 - beta1'*g1*beta1);
+            err = sum(sum(abs(g1_last - g1)))/sum(sum(abs(g1) + abs(g1_last)));
+            if  err < eps
+                break
+            end
+            %for faster convergence
+            g1 = 0.5 * (g1 + g1_last);
+            g1_last = g1;
+        end
+    
+        g2 = inv(alpha2);
+        g2_last = inv(alpha2);
+        err = 1;
+        for kk = 1:1000
+            g2 = inv((EE + 1i*eta)*eye(2) - alpha2 - beta2'*g2*beta2);
+            err = norm(g2 - g2_last,1)/norm(g2,1);
+            if  err < eps
+                break
+            end
+            %for faster convergence
+            g2 = 0.5 * (g2 + g2_last);
+            g2_last = g2;
+        end
+        
+        G_D = inv([inv(g1) -M;-M' inv(g2)]);
+        sigma1_corr = (1.0/(1 + exp(EE/kT))) * beta1' * (g1' - g1) * beta1;
+        sigma2_corr = (1.0/(1 + exp(EE/kT))) * beta2 * (g2' - g2) * beta2';
+        Sigma_D_corr = [sigma1_corr zeros(2,2);zeros(2,2) sigma2_corr];
+        G_corr = G_D * Sigma_D_corr * G_D';
+        
+        I_op = M * G_corr(3:4,1:2) - G_corr(1:2,3:4) * M;
+        I_E(1,jj) = I_op(1,1) - I_op(2,2);
+        
+        %Just to check with the direct formula
+        I_E(2,jj) = 4 * M0 * M0 * real(g1(2,1)*g2(1,2) - g2(2,1)*g1(1,2)) * 1.0/(1 + exp(EE/kT));
+    end
+    I_phi(1,ii) = (sum(I_E(1,:)));
+    I_phi(2,ii) = (sum(I_E(2,:)));
+end
+
+plot(phi_vec/(2*pi),I_phi(1,:),'LineWidth',2)
+hold on;
+plot(phi_vec/(2*pi),I_phi(2,:),'LineWidth',2)
+hold off;
+set(0,'DefaultTextInterpreter', 'latex');
+set(gca,'Fontsize',[16]);
+xlabel('$\frac{\phi}{2\pi}$','FontSize',16);
+ylabel('$I_J$ (arb units)','FontSize',16);
+title(strcat('$\Delta = \ $ ',num2str(Delta)));
+legend_handle = legend({'Current Operator','Direct Formula'},'Location','NorthEast');
+set(legend_handle,'Interpreter','latex');
+        
