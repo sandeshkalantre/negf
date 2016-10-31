fc780083016b20c8d6501adcc43beea5986b5072
Sandesh Kalantre <kalantresandesh@gmail.com>
2016-10-31 15:32:40 +0530

    BdG DOS Andreev level visible

--- MATLAB Codes/I_phi_N_device_point.m
+++ MATLAB Codes/I_phi_N_device_point.m
@@ -10,22 +10,21 @@ eta = 1e-8;
 
 %energies in eV
 t0 = 0.1;
-Delta = 0.005;
+Delta = 0.008;
 kT = 0.002;
 
 phi_vec = 2*pi*(0:0.05:1);
 I_phi = zeros(2,length(phi_vec));
 
 %tunelling
-%hopping parameter set equal to t0
 M0 = 0.1;
 M = M0 .* [1 0; 0 1];
 
 %device Hamiltonian
-U = 0;
-alpha = [2*t0 - U 0; 0 -2*t0 + U];
-beta1 = -t0* [1 0; 0 -1];
-H_D = alpha; 
+t1 = 0.1;
+alpha = [2*t1 0; 0 -2*t1];
+beta = -t1* [1 0; 0 -1];
+H_D = [alpha beta zeros(2);beta' alpha beta;zeros(2) beta' alpha]; 
 
 
 for ii = 1:length(phi_vec)
@@ -39,12 +38,8 @@ for ii = 1:length(phi_vec)
     alpha2 = [2*t0 Delta2; conj(Delta2) -2*t0];
     beta2 = -t0* [1 0; 0 -1];
 
-    E = 4 * t0 *(-1:0.0005:1);
-    I_E = zeros(1,length(E));
-    
-    %density of states
-    a_tot = zeros(1,length(E));
-    
+    E = 4 * t0 *(0:0.01:1);
+    I_E = zeros(2,length(E));
     for jj = 1:length(E)
         EE = E(jj);
     
@@ -69,7 +64,6 @@ for ii = 1:length(phi_vec)
             g2 = inv(alpha2);
             g2_last = inv(alpha2);
         end
-        
         err = 1;
         for kk = 1:1000
             g2 = inv((EE + 1i*eta)*eye(2) - alpha2 - beta2'*g2*beta2);
@@ -82,33 +76,35 @@ for ii = 1:length(phi_vec)
             g2_last = g2;
         end
         
-        g_D = inv((EE + 1i*eta) .* eye(2) - H_D);
+        g_D = inv((EE + 1i*eta) .* eye(6) - H_D);
         G_D = ...
-        inv([inv(g1) -M zeros(2,2);-M' inv(g_D) -M;zeros(2,2) -M' inv(g2)]);
+        inv([inv(g1) -[M zeros(2,6)];-[M ; zeros(4,2)] inv(g_D) -[zeros(4,2);M];-[zeros(2,6) M'] inv(g2)]);
         sigma1_corr = (1.0/(1 + exp(EE/kT))) * beta1' * (g1' - g1) * beta1;
         sigma2_corr = (1.0/(1 + exp(EE/kT))) * beta2 * (g2' - g2) * beta2';
         Sigma_D_corr = ...
-        [sigma1_corr zeros(2,2) zeros(2,2); 
-         zeros(2,2) zeros(2,2) zeros(2,2); 
-         zeros(2,2) zeros(2,2) sigma2_corr];
+        [sigma1_corr zeros(2,8); 
+         zeros(6,10); 
+         zeros(2,8) sigma2_corr];
      
         G_corr = G_D * Sigma_D_corr * G_D';
         
         I_op = M * G_corr(3:4,1:2) - G_corr(1:2,3:4) * M;
         I_E(1,jj) = I_op(1,1) - I_op(2,2);    
         
-        a(1,jj) = trace(1j * (g1 - g1'));
-        a(2,jj) = trace(1j * (g1 - g1'));
-        a_tot(1,jj) = trace(1j * (G_D - G_D'));
+        %calculation without device point, symbols resued, be careful in
+        %further use
+        G_D = inv([inv(g1) -M;-M' inv(g2)]);
+        sigma1_corr = (1.0/(1 + exp(EE/kT))) * beta1' * (g1' - g1) * beta1;
+        sigma2_corr = (1.0/(1 + exp(EE/kT))) * beta2 * (g2' - g2) * beta2';
+        Sigma_D_corr = [sigma1_corr zeros(2,2);zeros(2,2) sigma2_corr];
+        G_corr = G_D * Sigma_D_corr * G_D';
+        
+        I_op = M * G_corr(3:4,1:2) - G_corr(1:2,3:4) * M;
+        I_E(2,jj) = I_op(1,1) - I_op(2,2);
+        
     end
-    figure(1)
-    n1 = round(length(E) * 0.45);
-    n2 = round(length(E) * 0.55);
-    
-    %plot((1/t0) .* E(n1:n2),a_tot(n1:n2),'LineWidth',2);
-    
-    
     I_phi(1,ii) = sum(I_E(1,:));
+    I_phi(2,ii) = sum(I_E(2,:));
 end
 
 figure(1)
@@ -129,7 +125,4 @@ title(strcat('With Device Point $M = \ $ ',num2str(M0)));
 %title(strcat('Without Device Point $M = \ $ ',num2str(M0)));
 %legend_handle = legend({'With Device Point','Without Device Point'},'Location','NorthEast');
 %set(legend_handle,'Interpreter','latex');
-
-
-
         
